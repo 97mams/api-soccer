5a8f2767a6424562809ce8b88806643f89167aab
97mams <97mams@gmail.com>
2024-09-02 18:54:55 +0300

    add app.js fille

---
+++ app.js
@@ -0,0 +1,12 @@
+import { createServer } from "node:http";
+
+const port = 300;
+const host = "127.0.0.1";
+
+const callback = (request, response) => {
+}
+
+createServer(callback).listen(3000, (e) => {
+    if (e) throw new Error("misy olana");
+    console.log(`Project run in https://${host}:${port}/`);
+})

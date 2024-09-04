572cdbab8b40b70875f8ad08e70cd7ed36212bfb
97mams <97mams@gmail.com>
2024-09-04 17:51:23 +0300

    feat: add function jsonResponse

--- functions/api/soccer.js
+++ functions/api/soccer.js
@@ -7,30 +7,44 @@ export async function getTeams(resquest, respose) {
 
 export async function getTeam(resquest, respose, url) {
     const data = url.searchParams.get('name');
+    let message
     const teamName = data.toUpperCase();
     const findTeam = (await allTeam()).find(team => team.name === teamName.trim());
     if (!findTeam) {
-        return {
-            "message": "Team not found"
-        }
+        message = "Team not found"
+        return jsonResponse(message, findTeam)
     }
     return findTeam;
 }
 
 export async function addTeam(resquest, respose) {
     const teamName = await json(resquest)
+    let message = "Team successfully added";
+
     const teamfindTeam = (await allTeam()).find(team => team.name === teamName.name);
-    if (team) {
-        return {
-            message: "this name is also created"
-        };
+    if (teamfindTeam) {
+        message = "this name is also created"
+        return jsonResponse(message, teamfindTeam);
     }
-    return createTeam(teamName);
+
+    const newTeam = await createTeam(teamName);
+    return jsonResponse(message, newTeam);
+
 }
 
 export async function modifTeam(resquest, respose, url) {
     const id = url.searchParams.get('id');
     const params = await json(resquest)
-    const team = getTeams()
-    return updateTeam(team, id);
+    console.log(await updateTeam(params, id))
+}
+
+function jsonResponse(message, team) {
+    const json = {
+        message: message,
+        team: [
+            team
+        ]
+    };
+
+    return json;
 }

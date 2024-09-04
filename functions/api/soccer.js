d5b1c53c1f98e4d15287b12aa3999db0b5b82a34
97mams <97mams@gmail.com>
2024-09-04 06:54:49 +0300

    feat: add getTeam

--- functions/api/soccer.js
+++ functions/api/soccer.js
@@ -1,13 +1,18 @@
 import { allTeam, createTeam, updateTeam } from "../soccerStorage.js"
 import { json } from "node:stream/consumers";
 
-export function getTeams() {
+export function getTeams(resquest, respose) {
     return allTeam()
+}
+
+export function getTeam(resquest, respose, url) {
+    const id = parseInt(url.searchParams.get('id'), 10);
+    return findTeam(await json(resquest), id);
 }
 
 export async function addTeam(resquest, respose) {
     const teamName = await json(resquest)
-    const team = (await getTeams()).find(team => team.name === teamName.name);
+    const team = (await allTeam()).find(team => team.name === teamName.name);
     if (team) {
         return {
             message: "this name is also created"
@@ -18,5 +23,7 @@ export async function addTeam(resquest, respose) {
 
 export async function modifTeam(resquest, respose, url) {
     const id = url.searchParams.get('id');
-    return await updateTeam(await json(resquest), id);
+    const params = await json(resquest)
+    const team = getTeams()
+    return updateTeam(, id);
 }

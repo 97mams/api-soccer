8f6b178901aa791557d1bafa68f61c0e4c12bfb4
97mams <97mams@gmail.com>
2024-09-22 08:49:40 +0300

    update match

--- functions/api/Match.js
+++ functions/api/Match.js
@@ -42,15 +42,32 @@ const buildMacht = (teams) => {
     for (let team in teams) {
         const key = parseInt(team)
         const endValue = teams[key + 1] == undefined ? teams[0] : teams[key + 1];
-        result.push({ "team1": teams[key].name, "team2": endValue.name });
+        result.push({ "team1": teams[key].teamId, "team2": endValue.teamId });
     }
     return result
 }
 
-export function getMatch() {
+export const getMatch = () => {
     const result = []
     for (let group of groups) {
         result.push({ group: group.group, matchs: buildMacht(group.teams) })
+    }
+    return result
+}
+
+export function callandrie() {
+    const matchs = getMatch()
+    const result = [];
+    for (let key in matchs) {
+        const json = {
+            "day": key + 1,
+            "matchs": {
+                "groupe": matchs[key].group,
+                "teams": matchs[key].matchs[key]
+            }
+        }
+        result.push(json)
+
     }
     return result
 }

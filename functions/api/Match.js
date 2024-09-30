c91a37454f409671188fea5949b5d111725737d4
97mams <97mams@gmail.com>
2024-09-30 23:08:55 +0300

    fix generator match finished

--- functions/api/Match.js
+++ functions/api/Match.js
@@ -8,25 +8,19 @@ const buildMatch = (teams) => {
     for (let team in teams) {
         const Indexkey = parseInt(team)
         const exitLoop = teams.length
-
-
-        // result.push({ "team1": newArrayTeam[key].teamId, "team2": endValue.teamId });
         for (let key = Indexkey; key < exitLoop; key++) {
-            const endValue = teams[key + 1] == undefined ? teams[0] : teams[key + 1];
-            result.push([Indexkey, key + 1])
-            // result.push({ team1: teams[Indexkey].teamId, team2: endValue.teamId });
-
+            if (exitLoop !== key + 1) {
+                result.push({ team1: teams[Indexkey].teamId, team2: teams[key + 1].teamId })
+            }
         }
     }
-    console.log(result);
-
     return result
 }
 
 export const getMatch = () => {
     const result = []
     console.log(groups[3].teams)
-    result.push({ group: groups.group, matchs: buildMatch(groups[3].teams,) })
+    result.push({ group: groups.group, matchs: buildMatch(groups[0].teams,) })
     for (let group of groups) {
     }
     return result

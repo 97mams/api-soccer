daf732124364cac7003d3c59fdc0e7af5ec58b50
97mams <97mams@gmail.com>
2024-10-13 07:12:21 +0300

    feat model group type

---
+++ models/group_type.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class group_type extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  group_type.init({
+    name_type: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'group_type',
+  });
+  return group_type;
+};

37805345336e0418e125175b5211a9245b418c56
97mams <97mams@gmail.com>
2024-10-26 06:30:35 +0300

    add eslint

--- controllers/Match.js
+++ controllers/Match.js
@@ -2,13 +2,13 @@ const { jsonResponse } = require("../services/jsonResponseService");
 const { findAllMatchService, addMatchService } = require("../services/matchService");
 const { json } = require('node:stream/consumers')
 
-async function getMatches(request, response) {
+async function getMatches() {
     const matches = await findAllMatchService()
     const nameResponse = 'matches'
     return jsonResponse(nameResponse, matches)
 }
 
-async function addMatch(request, response) {
+async function addMatch(request) {
     const isGenerate = await json(request)
     await addMatchService(isGenerate)
 }

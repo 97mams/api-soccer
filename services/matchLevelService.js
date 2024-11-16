ee0c2acf0801eef509520c8850a394070e41b343
97mams <97mams@gmail.com>
2024-11-16 20:53:22 +0300

    fix query

--- services/matchLevelService.js
+++ services/matchLevelService.js
@@ -1,11 +1,20 @@
-const Team = require("../models");
+const { Op } = require("sequelize");
+const { Team } = require("../models");
 
 const builderRulesMatch = async (params) => {
     //
 }
 
 const findTeamByPoint = async () => {
-    return await Team.findOne()
+    return await Team.findAll(
+        {
+            where: {
+                point: {
+                    [Op.gte]: 3
+                }
+            }
+        }
+    )
 }
 
 module.exports = { findTeamByPoint };
